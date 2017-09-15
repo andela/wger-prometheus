8bd253e5610f7c305b24025f12c354b29f446afd
iankigen <ian.gabe.ian@gmail.com>
2017-09-15 17:05:02 +0300

    [fix] fixed linting errors

--- wger/weight/static/js/get_token.js
+++ wger/weight/static/js/get_token.js
@@ -2,7 +2,9 @@ var url = window.location.href;
 var origin = window.location.origin;
 
 try {
-  window.location.replace(origin + '/en/weight/fitbit/' + url.split('#')[1].split('=')[1].split('&')[0]);
+  window.location.replace(
+    origin + '/en/weight/fitbit/' + url.split('#')[1].split('=')[1].split('&')[0]
+  );
 } catch (err) {
   console.log(err);
 }
