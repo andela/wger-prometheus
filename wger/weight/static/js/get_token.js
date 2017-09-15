e2c1ca73ca4c8edfc2b840a92f324040a29061c6
iankigen <ian.gabe.ian@gmail.com>
2017-09-15 13:59:32 +0300

    [fix] fix JSlint issues

--- wger/weight/static/js/get_token.js
+++ wger/weight/static/js/get_token.js
@@ -1,13 +1,9 @@
 var url = window.location.href;
+var origin = window.location.origin;
 
-try{
-    var access_token = url.split('#')[1].split('=')[1].split('&')[0];
-    var origin = window.location.origin;
-    window.location.replace(origin + '/en/weight/fitbit/' + access_token);
-} catch(err){
+try {
+    var accessToken = url.split("#")[1].split("=")[1].split("&")[0];
+    window.location.replace(origin + "/en/weight/fitbit/" + accessToken);
+} catch (err) {
     console.log(err);
 }
-
-
-
-
