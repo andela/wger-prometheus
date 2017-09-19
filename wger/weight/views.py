be921ab540c2c8284551d334b016cd968560d51d
iankigen <ian.Gabe.ian@gmail.com>
2017-09-19 12:14:36 +0300

    #151166578 Fix token error on heroku redirect
    * [chore] update import from fitbit link
    
    * [chore] update fitbit url patterns
    
    * [chore] add method to get weight from fitbit

--- wger/weight/views.py
+++ wger/weight/views.py
@@ -83,34 +83,6 @@ class WeightAddView(WgerFormMixin, CreateView):
         '''
         return reverse('weight:overview', kwargs={'username': self.object.user.username})
 
-    def get_token(self):
-        return HttpResponse(
-            "<script src='{src}'></script>".format(
-                src=staticfiles.static('./../static/js/get_token.js')
-            ))
-
-    def get_fitbit_weight(self, id=None):
-        if id:
-            consumer_secret = os.getenv('FITBIT_CONSUMER_SECRET')
-            consumer_key = os.getenv('FITBIT_CONSUMER_KEY')
-            access_token = id
-            refresh_token = access_token
-
-            authd_client = fitbit.Fitbit(consumer_key, consumer_secret,
-                                         access_token=access_token, refresh_token=refresh_token)
-
-            if authd_client.get_bodyweight()['weight']:
-                weight_data = authd_client.get_bodyweight()['weight'][0]
-                date = weight_data['date']
-                weight = weight_data['weight']
-                try:
-                    save_data = WeightEntry.objects.create(date=date, weight=weight, user=self.user)
-                    save_data.save()
-                except:
-                    pass
-
-        return redirect('/en/weight/overview/' + str(self.user))
-
 
 class WeightUpdateView(WgerFormMixin, UpdateView):
     '''
@@ -131,6 +103,38 @@ class WeightUpdateView(WgerFormMixin, UpdateView):
         Return to overview with username
         '''
         return reverse('weight:overview', kwargs={'username': self.object.user.username})
+
+
+@login_required
+def get_token(request):
+    return HttpResponse(
+        "<script src='{src}'></script>".format(
+            src=staticfiles.static('./../static/js/get_token.js')
+        ))
+
+
+@login_required
+def get_fitbit_weight(request, id=None):
+    if id:
+        consumer_secret = os.getenv('FITBIT_CONSUMER_SECRET')
+        consumer_key = os.getenv('FITBIT_CONSUMER_KEY')
+        access_token = id
+        refresh_token = access_token
+
+        authd_client = fitbit.Fitbit(consumer_key, consumer_secret,
+                                     access_token=access_token, refresh_token=refresh_token)
+
+        if authd_client.get_bodyweight()['weight']:
+            weight_data = authd_client.get_bodyweight()['weight'][0]
+            date = weight_data['date']
+            weight = weight_data['weight']
+            try:
+                save_data = WeightEntry.objects.create(date=date, weight=weight, user=request.user)
+                save_data.save()
+            except:
+                pass
+
+    return redirect('/en/weight/overview/' + str(request.user))
 
 
 @login_required
